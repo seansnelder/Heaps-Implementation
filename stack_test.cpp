bd57af12b87b68d071884ed3897cbc8d54d612ee
Sean Snelder <spsnelde@usc.edu>
2023-02-24 03:24:03 +0000

    final changes

---
+++ stack_test.cpp
@@ -0,0 +1,18 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+  Stack o; 
+  cout << o.empty() <<endl; 
+
+  o.pop(3);
+  cout << o.empty() <<endl; 
+
+
+
+   1s
+
+
+}
